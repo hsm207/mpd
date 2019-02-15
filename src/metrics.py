0c09e1dcb9542d26dd0d14562d04b8f660ae1774
EC2 Default User <ec2-user@ip-172-16-31-70.ap-southeast-1.compute.internal>
2019-02-15 17:28:30 +0000

    Add more utilities to compute and evaluate metrics.

--- src/metrics.py
+++ src/metrics.py
@@ -1,4 +1,7 @@
 from sklearn.metrics import accuracy_score, f1_score, confusion_matrix
+import matplotlib.pyplot as plt
+import numpy as np
+import itertools
 
 
 def compute_eval_metrics(df):
@@ -10,7 +13,7 @@ def compute_eval_metrics(df):
         return {
             'accuracy': acc,
             'f1-macro': f1_macro,
-            'confusian_matrix': cf
+            'confusion_matrix': cf
         }
 
     malay_publications = ['bh', 'hm']
@@ -38,3 +41,38 @@ def print_metrics(metrics):
 
     print(f'EN accuracy:{metrics["en"]["accuracy"]:>16.4f}')
     print(f'EN f1-macro:{metrics["en"]["f1-macro"]:>16.4f}\n')
+
+    
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.tight_layout()
